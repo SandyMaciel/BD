3645624be0ec183f7153f7d5d45b0fa2594bf931
SandyMaciel <elleysandymaciel@gmail.com>
2016-05-10 19:25:35 -0300

    Atualizando script

--- SCRIPT_DB.sql
+++ SCRIPT_DB.sql
@@ -1,111 +1,129 @@
-CREATE DATABASE IPTU;
 
-CREATE TABLE IPTU(
-	N_PARCELAS INT NOT NULL,
-    VALOR_PARCELA VARCHAR(45),
-    DESCONTO VARCHAR(45),
-    DT_VENCIMENTO DATE,
-    DT_PAGAMENTNO DATE,
-    PRIMARY KEY (N_PARCELAS));
 
-CREATE TABLE AUTUACAO(
-	IMOVEL_AUTUADO boolean NOT NULL,
-    DESC_OCORRENCIA VARCHAR (45),
-    DT_COMPARECIMENTO DATE,
-    PRIMARY KEY (IMOVEL_AUTUADO));
-    
-CREATE TABLE BAIRRO(
-	NOME INT NOT NULL,
-    REPRESENTANTE VARCHAR(45),
-    TEL_CONTATO VARCHAR(45),
-    PRIMARY KEY(NOME));
-    
-CREATE TABLE PROPRIETARIO(
-	CPF VARCHAR(45) NOT NULL,
-    NOME INT,
-    ENDERECO VARCHAR(45),
-    SEXO VARCHAR(1),
-    DT_NASCIMENTO DATE,
-    PROPRIETARIOCOL VARCHAR(45),
-    primary key (CPF));
 
-CREATE TABLE OS(
-   ANO DATE NOT NULL,
-   ID_SEQUENCIA VARCHAR(45) NOT NULL,
-   DT_INICIAL DATE,	
-   DT_FINAL DATE,
-   IMOVEL_AUTUADO boolean NOT NULL,
-   primary key(ANO,ID_SEQUENCIA,IMOVEL_AUTUADO),
-   foreign key(IMOVEL_AUTUADO) references AUTUACAO(IMOVEL_AUTUADO)
-   );
+CREATE TABLE imovel (
+  inscricao INT(11) NOT NULL,
+  endereco VARCHAR(100) NOT NULL,
+  areaEdificada INT(11) NOT NULL,
+  areaTerreno INT(11) NOT NULL,
+  CPFproprietario INT(11) NOT NULL,
+  valorImovel INT(11) NULL DEFAULT NULL,
+  idTipoImovel INT(11) NOT NULL,
+  tipoImovel_idTipoImovel INT(11) NOT NULL,
+  nome_bairro VARCHAR(100) NOT NULL,
+  PRIMARY KEY (inscricao),
+ 
+    FOREIGN KEY (tipoImovel_idTipoImovel)
+    REFERENCES tipoImovel (idTipoImovel)
    
-CREATE TABLE FISCAL(
-	MATRICULA INT NOT NULL,
-    NOME VARCHAR(45),
-    DT_NASCIMENTO DATE,
-    DT_ADMISSAO DATE,
-    ENDERECO VARCHAR(45),
-    SEXO BOOLEAN NOT NULL,
-    ANO DATE NOT NULL,
-    ID_SEQUENCIA VARCHAR(45) NOT NULL,
-    IMOVEL_AUTUADO BOOLEAN NOT NULL,
-    PRIMARY KEY(MATRICULA,ANO,ID_SEQUENCIA,IMOVEL_AUTUADO),
-    foreign key(ANO,ID_SEQUENCIA,IMOVEL_AUTUADO)
-    references OS(ANO,ID_SEQUENCIA,IMOVEL_AUTUADO)
-);
+    FOREIGN KEY (nome_bairro)
+    REFERENCES bairro (nome_bairro)
+  
 
-CREATE TABLE IMOVEL(
-	INSCRICAO INT NOT NULL,
-    ENDERECO varchar(45),
-    AREA_EDIFICADA varchar(45),
-    AREA_DO_TERRENO varchar(45),
-    PROPRIETARIO varchar(45),
-    VALOR varchar(45),
-    TIPO_IMOVEL varchar(45),
-    BAIRRO_NOME int,
-    ANO DATE NOT NULL,
-    ID_SEQUENCIA VARCHAR(45) NOT NULL,
-    IMOVEL_AUTUADO BOOLEAN NOT NULL, 
-    primary key (INSCRICAO,BAIRRO_NOME,ANO,ID_SEQUENCIA,IMOVEL_AUTUADO),
-    foreign key(BAIRRO_NOME)
-    references BAIRRO (NOME),
-    foreign key (ANO,ID_SEQUENCIA,IMOVEL_AUTUADO)
-    references OS (ANO,ID_SEQUENCIA,IMOVEL_AUTUADO));
 
-CREATE TABLE `IPTU_has_IMOVEL` (
-  `IPTU_N_PARCELAS` INT NOT NULL COMMENT '',
-  `IMOVEL_INSCRICAO` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`IPTU_N_PARCELAS`, `IMOVEL_INSCRICAO`)  COMMENT '',
-  INDEX `fk_IPTU_has_IMOVEL_IMOVEL1_idx` (`IMOVEL_INSCRICAO` ASC)  COMMENT '',
-  INDEX `fk_IPTU_has_IMOVEL_IPTU1_idx` (`IPTU_N_PARCELAS` ASC)  COMMENT '',
-  CONSTRAINT `fk_IPTU_has_IMOVEL_IPTU1`
-    FOREIGN KEY (`IPTU_N_PARCELAS`)
-    REFERENCES  `IPTU` (`N_PARCELAS`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_IPTU_has_IMOVEL_IMOVEL1`
-    FOREIGN KEY (`IMOVEL_INSCRICAO`)
-    REFERENCES `IMOVEL` (`INSCRICAO`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE bairro (
+  nome_bairro VARCHAR(100) NOT NULL,
+  representante VARCHAR(100) NULL DEFAULT NULL,
+  telefone INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (nome_bairro))
 
-CREATE TABLE  `PROPRIETARIO_has_IMOVEL` (
-  `CPF` VARCHAR(45) NOT NULL COMMENT '',
-  `INSCRICAO` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`CPF`, `INSCRICAO`)  COMMENT '',
-  INDEX `fk_PROPRIETARIO_has_IMOVEL_IMOVEL1_idx` (`INSCRICAO` ASC)  COMMENT '',
-  INDEX `fk_PROPRIETARIO_has_IMOVEL_PROPRIETARIO1_idx` (`CPF` ASC)  COMMENT '',
-  CONSTRAINT `fk_PROPRIETARIO_has_IMOVEL_PROPRIETARIO1`
-    FOREIGN KEY (`CPF`)
-    REFERENCES  `PROPRIETARIO` (`CPF`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_PROPRIETARIO_has_IMOVEL_IMOVEL1`
-     FOREIGN KEY (`INSCRICAO`)
-    REFERENCES `IMOVEL` (`INSCRICAO`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE proprietario (
+  CPF_proprietario INT(11) NOT NULL,
+  nome VARCHAR(100) NOT NULL,
+  end_imovel_reside VARCHAR(100) NOT NULL,
+  sexo VARCHAR(1) NOT NULL,
+  dt_nascimento DATE NOT NULL,
+  PRIMARY KEY (CPF_proprietario))
 
+
+CREATE TABLE iptu(
+  imovel_inscricao` INT(11) NOT NULL,
+  ano INT(11) NOT NULL,
+  n_parcela`INT(11) NOT NULL,
+  dt_vencimento DATE NOT NULL,
+  valorParcela INT(11) NOT NULL,
+  valorDesconto INT(11) NULL DEFAULT NULL,
+  dt_pagamentoRealizado INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (imovel_inscricao, ano, n_parcela),
+  
+    FOREIGN KEY (imovel_inscricao)
+    REFERENCES imovel (inscricao)
+    )
+
+
+CREATE TABLE os (
+  ano INT(11) NOT NULL,
+  idSequencia INT(11) NOT NULL,
+  dt_inicio DATE NOT NULL,
+  dt_final DATE NOT NULL,
+  fiscal1_matricula INT(11) NOT NULL,
+  fiscal2_matricula INT(11) NOT NULL,
+  PRIMARY KEY (ano, idSequencia),
+  
+    FOREIGN KEY (fiscal1_matricula)
+    REFERENCES fiscal (matricula)
     
+    FOREIGN KEY (fiscal2_matricula)
+    REFERENCES fiscal (matricula)
+   )
+
+
+CREATE TABLE fiscal (
+  matricula INT(11) NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  dt_nascimento DATE NOT NULL,
+  dt_admissao DATE NOT NULL,
+  sexo VARCHAR(1) NOT NULL,
+  endereco VARCHAR(45) NOT NULL,
+  PRIMARY KEY (matricula))
+
+
+CREATE TABLE tipoImovel (
+  idTipoImovel INT(11) NOT NULL AUTO_INCREMENT,
+  tipoImovel VARCHAR(50) NOT NULL,
+  PRIMARY KEY (idTipoImovel`),
+  UNIQUE  (idTipoImovel))
+
+
+CREATE TABLE autuacao (
+  imovel_inscricao` INT(11) NOT NULL,
+  os_idSequencia INT(11) NOT NULL,
+  os_ano INT(11) NOT NULL,
+  incricaoImovel VARCHAR(45) NOT NULL,
+  descricao INT(11) NULL DEFAULT NULL,
+  dt_comparecimento VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (imovel_inscricao, os_idSequencia, os_ano),
+  
+    FOREIGN KEY (imovel_inscricao , os_ano , os_idSequencia)
+    REFERENCES imovel_has_os (imovel_inscricao , os_ano , os_idSequencia)
+    )
+
+
+CREATE TABLE imovel_has_proprietario (
+  inscricao INT(11) NOT NULL,
+  CPF_proprietario INT(11) NOT NULL,
+  PRIMARY KEY (inscricao, CPF_proprietario),
+  
+    FOREIGN KEY (inscricao)
+    REFERENCES imovel (inscricao)
    
-   
+    FOREIGN KEY (CPF_proprietario)
+    REFERENCES proprietario(CPF_proprietario)
+   )
+
+CREATE TABLE imovel_has_os(
+  imovel_inscricao INT(11) NOT NULL,
+  os_ano INT(11) NOT NULL,
+  os_idSequencia INT(11) NOT NULL,
+  PRIMARY KEY (imovel_inscricao, os_ano, os_idSequencia),
+  
+    FOREIGN KEY (imovel_inscricao)
+    REFERENCES imovel (inscricao)
+    
+    FOREIGN KEY (os_ano , os_idSequencia)
+    REFERENCES os (ano , idSequencia)
+   )
+
+
+
+
